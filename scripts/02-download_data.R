166e4cd62ef2f66d2e988fb81290eabeacefef67
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-11-25 02:50:32 +0000

    Download raw data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Yizhe Chen
+# Date: 24 Nov 2024
+# Contact: yz.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Install `opendatatoronto` and `tidyverse` R packages
+# Any other information needed? No
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Get package details
+package <- show_package("toronto-beaches-observations")
 
+# List all resources for this package
+resources <- list_package_resources("toronto-beaches-observations")
+
+# Filter for datastore resources (CSV or GeoJSON formats)
+datastore_resources <- resources |>
+  filter(tolower(format) %in% c('csv', 'geojson'))
+
+# Load the first datastore resource (CSV file for observations)
+the_raw_data <- datastore_resources |>
+  filter(row_number() == 1) |>
+  get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# Save the downloaded raw data as a CSV file
+write_csv(the_raw_data, "data/01-raw_data/raw_data.csv")
